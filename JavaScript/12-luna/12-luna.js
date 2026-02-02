a3ae4306e2e88e27541adafcbd1d021e7c0adb14
Timur <tim.sam6396@gmail.com>
2026-02-02 18:26:39 +0500

    Луна исправлено

--- JavaScript/12-luna/12-luna.js
+++ JavaScript/12-luna/12-luna.js
@@ -1,18 +1,16 @@
 const card = "4561-2612-1234-5464";
 
-const cardNumber = card.replaceAll("-", "").split("").map(Number);
+function checkCard(card) {
+  const cardNumber = card.replaceAll("-", "").split("").map(Number);
 
-const checkSum = cardNumber.reduce((acc, num, i) => {
-  if (i % 2 === 0) {
-    let doubled = num * 2;
-    if (doubled > 9) doubled -= 9;
-    return acc + doubled;
-  }
-  return acc + num;
-}, 0);
-
-if (checkSum % 10 === 0) {
-  console.log("Номер карты корректен");
-} else {
-  console.log("Ошибка в номере карты");
+  const checkSum = cardNumber.reduce((acc, num, i) => {
+    if (i % 2 === 0) {
+      let doubled = num * 2;
+      if (doubled > 9) doubled -= 9;
+      return acc + doubled;
+    }
+    return acc + num;
+  }, 0);
+  return checkSum % 10 === 0;
 }
+console.log(checkCard(card));
